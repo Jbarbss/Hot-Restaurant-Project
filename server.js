7fe02d354d5408ec7573f6efbaf506da6ba2a235
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-04-17 13:13:24 -0400

    added to server

--- server.js
+++ server.js
@@ -1,20 +1,80 @@
-const express = require("express");
-const path = require("path");
+var bodyParser = require("body-parser");
+var path = require("path");
+const express = require('express');
 
-const reservationList = [];
-const waitList = [];
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-const app = express();
-const PORT = 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-function Table(name, phoneNumber, email, id) {
-  this.name = name;
-  this.phoneNumber = phoneNumber;
-  this.email = email;
-  this.id = id;
-}
+// Storing all the tables
+var data = {
+	reservations: [],
+	waitlist: [],
+};
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+var visitorCount = 0;
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+// Routing
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+  visitorCount++;
+});
+
+app.get("/reserve.html", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables.html", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+// Get reservation data via the api
+app.get("/api/.html", function(req, res) {
+  res.json(data.reservations);
+});
+
+app.get("/api/waitlist.html", function(req, res) {
+  res.json(data.waitlist);
+});
+
+// Returns both the tables array and the waitlist array
+app.get("/api/", function(req, res) {
+  res.json(data);
+});
+
+app.get("/api/clear", function(req, res) {
+  data.reservations.length = 0;
+  data.waitlist.length = 0;
+  res.json(data);
+});
+
+app.get("/api/visitors", function(req, res) {
+  res.json(visitorCount);
+});
+
+// Get new table data entry from POST
+app.post("/api/new", function(req, res) {
+  var tableData = req.body;
+  console.log(tableData);
+  if (tableData && tableData.name) {
+  	tableData.routeName = tableData.name.replace(/\s+/g, "").toLowerCase();
+  }
+  console.log(tableData);
+
+  if (data.reservations.length < 5) {
+  	data.reservations.push(tableData);
+  } else {
+  	data.waitlist.push(tableData);
+  }
+  
+
+  res.json(tableData);
+});
+
+
+// Start the Server
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
